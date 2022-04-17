59255357b36d9780cc70353e0fd7b5fc0e5cb5be
hiwa2719 <hiahmadyan@gmail.com>
2022-04-17 17:02:17 +0430

    added Hash model

--- hashing/models.py
+++ hashing/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Hash(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    text = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text[:100]
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=('user', 'text'), name='user_text_uniqueness')
+        ]
