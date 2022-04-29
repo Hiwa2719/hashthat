70d862e672ee12c9bc7920f6d804f8dabd117efa
hiwa2719 <hiahmadyan@gmail.com>
2022-04-29 15:42:39 +0430

    add authentication status and it's buttons

--- frontend/src/components/IndexPage.js
+++ frontend/src/components/IndexPage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from 'axios'
+import {Link} from 'react-router-dom'
 
 
 export default class IndexPage extends React.Component {
@@ -38,17 +39,28 @@ export default class IndexPage extends React.Component {
         this.inputRef.current.value = ''
     }
 
-    loginHandler = () =>{
+    loginHandler = () => {
         this.props.toggleOpenModal(<h1>hello world</h1>)
     }
 
     render() {
-        const {toggleOpenModal} = this.props
+        const {toggleOpenModal, isAuthenticated} = this.props
         return (
-            <div className="w-50 d-flex justify-content-center h-75">
+            <div className="index-page w-50 d-flex justify-content-center h-75">
                 <div className="text-light bg-secondary w-75 p-1 rounded-1">
                     <div className="text-end pe-3 mb-3" onClick={this.loginHandler}>
-                        Login / Register
+                        {isAuthenticated ?
+                            (
+                                <div>
+                                    <Link to='/account/'>My Account</Link> / <span>Logout</span>
+                                </div>
+                            ) :
+                            (
+                                <div>
+                                    <span>Login</span> / <span>Register</span>
+                                </div>
+                            )
+                        }
                     </div>
                     <h1 className="text-center my-3">Please Enter your Text</h1>
                     <textarea className="w-100 text-area mt-3" placeholder="Enter Your text here"
