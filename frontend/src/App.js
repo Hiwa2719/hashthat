b8f32c4d56592ba486f4223c5ca8979085194a1a
hiwa2719 <hiahmadyan@gmail.com>
2022-04-29 12:07:39 +0430

    re importing frontend folder to git

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import IndexPage from "./components/IndexPage";
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
+function App() {
+    return (
+        <div className="App">
+            <Router>
+                <div className="vh-100 d-flex align-items-center justify-content-center bg-dark">
+                    <Routes>
+                        {/*<Route path="/" element={<Account/>}/>*/}
+                        <Route path="/" element={<IndexPage/>}/>
+                    </Routes>
+                </div>
+            </Router>
+        </div>
+    );
+}
+
+export default App;
---
+++ frontend/src/components/IndexPage.js
@@ -0,0 +1,61 @@
+import React from "react";
+import axios from 'axios'
+
+
+export default class IndexPage extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            hash: null,
+        }
+        this.inputRef = React.createRef()
+    }
+
+    generateHash = () => {
+        axios.get('/generate_hash/')
+            .then(response => {
+                this.setState({
+                    hash: response.data,
+                })
+            })
+            .catch(error => {
+                console.log('Error')
+                console.log(error)
+            })
+    }
+    saveHashHandler = () => {
+        axios.post('/save-text-hash/', this.state.text)
+            .then(response => {
+                alert('Your text has been saved')
+            })
+            .catch(error => {
+                console.log('Error')
+                console.log(error)
+            })
+
+    }
+    clearHandle = () => {
+        this.inputRef.current.value = ''
+    }
+
+    render() {
+        return (
+            <div className="w-50 d-flex justify-content-center h-75">
+                <div className="text-light bg-secondary w-75 p-1 rounded-1">
+                    <div className="text-end pe-3 mb-3">
+                        Login / Register
+                    </div>
+                    <h1 className="text-center my-3">Please Enter your Text</h1>
+                    <textarea className="w-100 text-area mt-3" placeholder="Enter Your text here"
+                              ref={this.inputRef}></textarea>
+                    <div className="hash-box"></div>
+                    <div className="text-center mt-2">
+                        <button className="mx-1 btn btn-success" onClick={this.generateHash}>Generate Hash</button>
+                        <button className="mx-1 btn btn-warning" onClick={this.saveHashHandler}>Save Hash</button>
+                        <button className="mx-1 btn btn-primary" onClick={this.clearHandle}>Clear Form</button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
