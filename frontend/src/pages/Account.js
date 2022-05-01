e08eeb083bedba5d642cd4450157b8d46d17ccbf
hiwa2719 <hiahmadyan@gmail.com>
2022-05-01 13:23:59 +0430

    adding logout functionality inside Account.js

--- frontend/src/pages/Account.js
+++ frontend/src/pages/Account.js
@@ -1,14 +1,23 @@
 import React from "react";
 import {Link} from 'react-router-dom'
+import axios from "axios";
 
 export default class Account extends React.Component {
+    logoutHandler = () => {
+        axios.get('/logout/')
+            .catch(error => {
+                console.log('error')
+                console.log(error)
+            })
+    }
+
     render() {
         return (
             <div>
                 <Link to="/" className="btn btn-secondary">Home</Link>
                 <button className="text-hash-list btn btn-success mx-1 my-3">Display saved Text/Hash</button>
                 <button className="pass-change btn btn-primary mx-1 my-3">Change password</button>
-                <Link className="logout btn btn-warning mx-1 my-3" to="/">logout</Link>
+                <Link className="logout btn btn-warning mx-1 my-3" to="/" onClick={this.logoutHandler}>logout</Link>
                 <Link to="/" className="btn btn-danger mx-1 my-3">Delete Account</Link>
             </div>
         )
